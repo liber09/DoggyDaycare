40e5e86ca54bd89a5ec410584e4952c93c70162d
liber09 <linda.bergsangel@hotmail.com>
2023-09-21 04:37:16 +0200

    Styling for logo mobile/desktop

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Header() {
     return (
       <main className={styles.header}>
-        <Image src={"/../images/cartoon-dogs.png"} alt="Logo with 5 cartoon dogs" width="200" height="136"></Image>
+        <Image className={styles.logo} src={"/../images/cartoon-dogs.png"} alt="Logo with 5 cartoon dogs" width="100" height="68"></Image>
         <p className={styles.title}>Doggy Daycare</p>
       </main>
     )

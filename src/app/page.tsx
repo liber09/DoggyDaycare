f55868e1f61550cbc80879c2a37c1f59d3a48f5e
liber09 <linda.bergsangel@hotmail.com>
2023-09-22 04:04:30 +0200

    change from css to sass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.scss'
 import Header from '@/components/header/page'
 
 export default function Home() {
--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -1,13 +1,13 @@
-import styles from './page.module.css'
+import styles from './page.module.scss'
 import Image from 'next/image'
 import Menu from '../menu/page'
 
 export default function Header() {
     return (
       <main className={styles.header}>
-        <Menu></Menu>
         <Image className={styles.logo} src={"/../images/cartoon-dogs.png"} alt="Logo with 5 cartoon dogs" width="100" height="68"></Image>
         <p className={styles.title}>Doggy Daycare</p>
+        <Menu></Menu>
       </main>
     )
   }

45d8fc49c50952e93982b2cd279df2babfcc59d6
liber09 <linda.bergsangel@hotmail.com>
2023-09-22 05:49:29 +0200

    Register page title and style

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,6 +2,10 @@ import styles from './page.module.scss'
 
 export default function Catalog() {
     return (
-    <p>Register dog page</p>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Register new dog</h1>
+            <p>Here you can register new dogs that arrive to our daycare center</p>
+        </div>
+        
     )
 }

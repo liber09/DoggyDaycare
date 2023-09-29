2263228c3994c7befa4b67ed46b346f54152580b
liber09 <linda.bergsangel@hotmail.com>
2023-09-29 22:43:17 +0200

    moved account icon to the right

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -5,20 +5,20 @@ import Link from 'next/link'
 export default function Header() {
     return (
       <main className={styles.header}>
+        <Link href={"/"}>
+          <Image className={styles.logo} 
+            src={"/../images/logo.png"} 
+            alt="Logo with a puppy dog playing with a butterfly" 
+            width="500" 
+            height="500">
+          </Image>
+        </Link>
         <Link href={"/login"}>
           <Image className={styles.login} 
             src={"/../images/login.png"}
             alt="Login icon" 
             width="100" 
             height="100">
-          </Image>
-        </Link>
-        <Link href={"/"}>
-          <Image className={styles.logo} 
-            src={"/../images/logo.png"} 
-            alt="Logo with 5 cartoon dogs" 
-            width="500" 
-            height="500">
           </Image>
         </Link>
         

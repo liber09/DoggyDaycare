5aabacb9624decbbc9cbe6303bcabffef5937e54
liber09 <linda.bergsangel@hotmail.com>
2023-10-03 17:31:25 +0200

    menu and footer issues fixed

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,8 +2,10 @@ import styles from './footer.module.scss'
 
 export default function Footer() {
     return (
+      <div className={styles.container}>
       <footer className={styles.footer}>
         <p>(c)2023 Happy tails doggy daycare</p>
       </footer>
+      </div>
     )
   }
--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -9,6 +9,11 @@ export default function Menu() {
     function handleClick(this: any){
         setIsOpen(!isOpen);
     }
+    function handleLinkClick(){
+        if(isOpen){
+            setIsOpen(false);
+        }
+    }
 
 
 
@@ -16,10 +21,10 @@ export default function Menu() {
     <nav className={isOpen === false ? styles.main_nav : styles.main_nav_open}>
         <button onClick={handleClick} className={isOpen === false ? styles.main_nav_toggle : styles.main_nav_toggle_open}></button>
         <menu className={isOpen === false ? styles.main_menu : styles.main_menu_open}>
-            <Link onClick={handleClick} className={styles.link} href={"/catalog"}><li className={isOpen === false ? styles.main_menu_item : styles.main_menu_item_open}>Catalog</li></Link>
-            <Link onClick={handleClick} className={styles.link} href={"/register"}><li className={isOpen === false ? styles.main_menu_item : styles.main_menu_item_open}>Register dog</li></Link>
-            <Link onClick={handleClick} className={styles.link} href={"/employees"}><li className={isOpen === false ? styles.main_menu_item : styles.main_menu_item_open}>Employees</li></Link>
-            <Link onClick={handleClick} className={styles.link} href={"/schedule"}><li className={isOpen === false ? styles.main_menu_item : styles.main_menu_item_open}>Schedule</li></Link>
+            <Link onClick={handleLinkClick} className={styles.link} href={"/catalog"}><li className={isOpen === false ? styles.main_menu_item : styles.main_menu_item_open}>Catalog</li></Link>
+            <Link onClick={handleLinkClick} className={styles.link} href={"/register"}><li className={isOpen === false ? styles.main_menu_item : styles.main_menu_item_open}>Register dog</li></Link>
+            <Link onClick={handleLinkClick} className={styles.link} href={"/employees"}><li className={isOpen === false ? styles.main_menu_item : styles.main_menu_item_open}>Employees</li></Link>
+            <Link onClick={handleLinkClick} className={styles.link} href={"/schedule"}><li className={isOpen === false ? styles.main_menu_item : styles.main_menu_item_open}>Schedule</li></Link>
         </menu>
     </nav>
     )
